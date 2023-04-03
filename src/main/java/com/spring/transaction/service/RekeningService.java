053eecc02405dd77f55c8960c4564cf14ea8f4c3
Dandelions16 <doyokbsk@gmail.com>
2023-04-03 22:50:09 +0700

    docs(documentation code):

--- src/main/java/com/spring/transaction/service/RekeningService.java
+++ src/main/java/com/spring/transaction/service/RekeningService.java
@@ -30,11 +30,15 @@ public class RekeningService {
         }
         rekening1.setSaldo(rekening1.getSaldo()-amount);
         rekeningRepo.save(rekening1);
+//        Rekening 1 atau pengirim mengecek terlebih dahulu apakkah rekening ada atau tidak jika
+//        Rekening 1 ada maka proses tranfer akan berlanjut dengan mengurangi saldo rekening1
 
         RekeningEntity rekening2 = rekeningRepo.findByNoRek(norek2);
         if (rekening2 == null){
             throw new RuntimeException("Norek 2 not valid");
         }
         rekening2.setSaldo(rekening2.getSaldo()+amount);
+//        Jika rekening 2 tidak kosong atau tidak ada maka rekening 2 akan menerima saldo dari pengirim
+//        atau dari rekening 1
     }
  }
